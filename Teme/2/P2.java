8f2f5a032502f04d5d90886c7a24a41cc1462b22
Paul Hondola <paulhondola@gmail.com>
2024-10-07 11:41:36 +0300

    Update Teme/2/P2

--- Teme/2/P2.java
+++ Teme/2/P2.java
@@ -1,12 +1,20 @@
 // 2. Creat¸i o clas˘a ce cont¸ine dou˘a atribute nestatice private, un int ¸si un char care nu sunt init¸ializate ¸si tip˘arit¸i valorile acestora pentru a verifica dac˘a Java realizeaz˘a init¸ializarea implicit˘a.
 
-class P2 {
+class IDK {
 
 	private int myInt;
 	private char myChar;
 
-	public static void main(String[] args) {
+	public void print() {
 		System.out.println(myInt);
 		System.out.println(myChar);
+	}
+}
+
+class P2 {
+
+	public static void main(String[] args) {
+		IDK p = new IDK();
+		p.print();
 	}
 }
