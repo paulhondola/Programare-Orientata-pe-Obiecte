fcbaf9080885dd0cb54a356795e1674d334d38b1
Paul Hondola <paulhondola@gmail.com>
2024-12-11 15:00:34 +0200

    added lab 11

--- Lab/11/src/Main.java
+++ Lab/11/src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private List<String> list = new ArrayList<>();
+
+    public Main() {
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+
+        for (String str : list) {
+            System.out.println(str);
         }
+
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
     }
 }
